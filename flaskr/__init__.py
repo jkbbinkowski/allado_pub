46d328786a0de560c6417c22a8410a86cd332e09
Jakub Binkowski <jkb.binkowski@gmail.com>
2025-08-24 01:52:31 +0200

    fc

---
+++ flaskr/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# create app
+app = Flask(__name__, instance_relative_config=True)
+
+# configure app
+app.config['TESTING'] = False
+app.config['DEBUG'] = False
+app.config['FLASK_ENV'] = 'production'
+app.config['SECRET_KEY'] = 'y7i2ehfdpqmvvaic7232ba6ffb65568a224y7fsy7fc7wqrci7mfspqwqh7823nf'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
