3266b55974b6c4fdba26c5056cfb8fafccd0ae07
Jakub Binkowski <jkb.binkowski@gmail.com>
2025-08-24 07:21:22 +0200

    Remove sensitive configuration settings from Flask app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,6 +7,3 @@ app = Flask(__name__, instance_relative_config=True, static_url_path='/')
 app.config['TESTING'] = False
 app.config['DEBUG'] = False
 app.config['FLASK_ENV'] = 'production'
-app.config['SECRET_KEY'] = 'y7i2ehfdpqmvvaic7232ba6ffb65568a224y7fsy7fc7wqrci7mfspqwqh7823nf'
-app.config['SESSION_COOKIE_SECURE'] = True
-app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
